f72058502df3c99e8383927099709153951746c6
Krishna Pranay Angara <krishnapranay.angara@gmail.com>
2025-03-30 22:08:48 -0400

    Added basic header

--- App.js
+++ App.js
@@ -1,23 +1,67 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+import logo2 from "./src/assets/logo2.jpg";
 
 // ReactElement-JS Object => HTMLElement(rendered on the browser)
 // const heading = React.createElement("h1", { id: "heading" }, "Learning React by Building a Project 🧑🏽‍💻!!");
 
 
 // JSX => React.createElement => ReactElement-JS Object => HTMLElement(rendered on the browser)
-const jsxheading = <h1 id="heading">Namaste H1 using JSX!! 😎</h1>;
+
+/**
+ * 
+ * Header
+ *  - logo 
+ *  - Nav Items
+ * 
+ * Body 
+ * - Search
+ * - Restaurant List
+ *   - Restaurant Card
+ *  
+ * Footer
+ * - Copyright Information
+ * - Links to Social Media
+ * - Address
+ * - Contact Information
+ * 
+ */
+
+const Header = () => {
+    return (
+        <div className = "header">
+            <div>
+                <img className="logo" src={logo2} alt="Logo image"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                    
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            
+        </div>
+    );
+}
+
+const AppLayout = () =>  {
+    return(
+        <div className = "app">
+            <Header/>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-
-//React Functional Component
-const TitleComponent =() => { return <h1>Title component for Namaste react!!</h1> };
-const HeadingComponent = () => (
-    <div id="Container">
-        {jsxheading}
-        {TitleComponent()}
-        <h2 className="heading"> Namaste React Functional Component!! 😀</h2>
-    </div>
-);
-
-root.render(<HeadingComponent />);
+root.render(AppLayout());
